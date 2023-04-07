9f1cbbfd880f9210647e17005e4ff2e472ad35db
Smoopfrog <jeffstinson@hotmail.com>
2023-04-07 15:01:56 -0700

    add date input validation

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,8 +1,25 @@
 const ageCalculator = (event) => {
   event.preventDefault();
+  
   const inputDay = document.getElementById("input-day").value;
   const inputMonth = document.getElementById("input-month").value - 1;
   const inputYear = document.getElementById("input-year").value;
+  const currentYear = new Date().getFullYear();
+
+  if (inputDay < 1 || inputDay > 31 || !inputDay) {
+    console.log("day error");
+    return;
+  }
+
+  if (inputMonth < 0 || inputMonth > 11) {
+    console.log("month error");
+    return;
+  }
+
+  if (inputYear > currentYear || !inputYear) {
+    console.log("year error", currentYear);
+    return;
+  }
 
   const birthDate = new Date(inputYear, inputMonth, inputDay);
   const dateDiff = Date.now() - birthDate.getTime();
